154f0839c4f3bc58953d5227f9e73d227aca4708
ivan8882004 <ivan.cheung.yui@gmail.com>
2024-06-11 23:47:38 +0800

    updated project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,35 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'HKU Course Planner',
+    link: 'https://ivan8882004.github.io/HKUCoursePlanner-Reviewer/',
+    subtitle: 'Study Plan, Syllabus Roadmap and Timetable',
+    image: '/images/projects/courseplanner.jpg',
+    date: '2023-09-01',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'Study Plan, '
+      + 'Syllabus Roadmap, '
+      + 'Timetable, '
+      + 'Made with friends.',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    title: 'Kaggle Competition',
+    subtitle: 'On the road to Competition Master!',
+    link: 'https://www.kaggle.com/',
+    image: '/images/projects/kagglee.jpg',
+    date: '2024-05-20',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      'On the road to Competition Master!',
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
+    title: 'Trading/investing',
+    subtitle: 'Learning',
+    image: '/images/projects/trading.jpg',
+    date: '2023-06-28',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'Learning'
+      + 'Macro/Fundamental/Quant'
+      + '',
   },
 ];
 
